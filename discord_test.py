da550cff50db15428b4e1befe2e6920353d31d7a
dongdongzhuzhu <58351205+dongdongzhuzhu@users.noreply.github.com>
2022-11-17 14:53:21 +0800

    Update discord_test.py

--- discord_test.py
+++ discord_test.py
@@ -72,8 +72,8 @@ def chat(chanel_list,authorization):
 
 
 if __name__ == "__main__":
-    chanel_list = os.environ['chanel_list1']  # 这里是群聊号（url最右边）
-    authorization_list = os.environ['authorization_list1'] # 这里auth认证信息
+    chanel_list = os.environ['CHANEL_LIST1']  # 这里是群聊号（url最右边）
+    authorization_list = os.environ['AUTHORIZATION_LIST1'] # 这里auth认证信息
     while True:
         try:
             chat(chanel_list,authorization_list)
